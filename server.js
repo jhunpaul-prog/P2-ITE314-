a31dddd1477ed392bfb679ba90729eadd0568de8
jhunpaul-prog <jhunpaul331@gmail.com>
2024-09-10 09:48:23 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+// Route for /subject
+app.get('/subject', (req, res) => {
+    res.send('ITE314');
+});
+
+// Route for /exam
+app.get('/exam', (req, res) => {
+    res.send('P2');
+});
+
+// Route for /node
+app.get('/node', (req, res) => {
+    res.send('NODEJS');
+});
+
+// Route for /mydata
+app.get('/mydata', (req, res) => {
+    res.json({
+        subject: 'ITE314',
+        exam: 'P2',
+        node: 'NODEJS'
+    });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
